8a60d540d2c858849597af674432bafa751b61c3
Daniil Kupchenko <kupchenko@gmail.com>
2019-06-12 13:56:01 +0300

    tests for ipsec have been added

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,4 +1,5 @@
 import os
+import socket
 
 import testinfra.utils.ansible_runner
 
@@ -6,19 +7,21 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
-
-
 def test_ipsec_service_running_and_enabled(host):
     ipsec = host.service("ipsec")
     assert ipsec.is_running
     assert ipsec.is_enabled
 
+
+def test_ipsec_verify(host):
+    with host.sudo():
+        ipsec_verify = host.run("ipsec verify")
+    assert ipsec_verify.rc == 0
+
+
 def test_ipsec_connection_status(host):
-    command = host.command('ping -c1 -W1 192.168.2.1')
-    assert '1 packets transmitted, 1 received, 0% packet loss' in command.stdout
+    hostname = socket.gethostname()
+    test_ip = '192.168.2.1' if hostname == 'test-gw-1' else '192.168.1.1'
+    command = host.run('ping -c1 -W1 ' + test_ip)
     assert command.rc == 0
+    assert '1 packets transmitted, 1 received' in command.stdout
